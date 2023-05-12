c342832e1c13d2eee82c0c699f9f371076d51f40
Jayden <132095916+J-Mbatia@users.noreply.github.com>
2023-05-12 13:59:37 -0500

    Update generatorController.py

--- generatorController.py
+++ generatorController.py
@@ -28,7 +28,7 @@ class Controller(QMainWindow, Ui_GeneratorWindow):
             elif password_length > 64:
                 raise SyntaxError
 
-            words = ''
+            words = []
             word1_len = 0
             word2_len = 0
             word3_len = 0
@@ -59,7 +59,7 @@ class Controller(QMainWindow, Ui_GeneratorWindow):
                         word1 = linecache.getline('words_alpha.txt', random_word1)
                         word1_len = (len(word1) - 1)
                 extra_password_length -= word1_len
-                words += word1.strip('\n') + " "
+                words.append(word1.strip('\n'))
 
             if (password_length >= 16) and (password_length <= 64):
                 random_word2 = random.randint(1, file_length)
@@ -71,7 +71,7 @@ class Controller(QMainWindow, Ui_GeneratorWindow):
                         word2 = linecache.getline('words_alpha.txt', random_word2)
                         word2_len = (len(word2) - 1)
                 extra_password_length -= word2_len
-                words += word2.strip('\n') + " "
+                words.append(word1.strip('\n'))
 
             if (password_length >= 30) and (password_length <= 64):
                 random_word3 = random.randint(1, file_length)
@@ -83,7 +83,7 @@ class Controller(QMainWindow, Ui_GeneratorWindow):
                         word3 = linecache.getline('words_alpha.txt', random_word3)
                         word3_len = (len(word3) - 1)
                 extra_password_length -= word3_len
-                words += word3.strip('\n') + " "
+                words.append(word1.strip('\n'))
 
             if (password_length >= 41) and (password_length <= 64):
                 random_word4 = random.randint(1, file_length)
@@ -95,7 +95,7 @@ class Controller(QMainWindow, Ui_GeneratorWindow):
                         word4 = linecache.getline('words_alpha.txt', random_word4)
                         word4_len = (len(word4) - 1)
                 extra_password_length -= word4_len
-                words += word4.strip('\n') + " "
+                words.append(word1.strip('\n'))
 
             if (password_length >= 55) and (password_length <= 64):
                 random_word5 = random.randint(1, file_length)
@@ -107,11 +107,10 @@ class Controller(QMainWindow, Ui_GeneratorWindow):
                         word5 = linecache.getline('words_alpha.txt', random_word5)
                         word5_len = (len(word5) - 1)
                 extra_password_length -= word5_len
-                words += word5.strip('\n') + " "
+                words.append(word1.strip('\n'))
 
             word_len = word1_len + word2_len + word3_len + word4_len + word5_len
 
-            self.displayGenerator.setText(f'{words}')
 
         except SyntaxError:
             self.displayGenerator.setText('INVALID')
@@ -120,7 +119,7 @@ class Controller(QMainWindow, Ui_GeneratorWindow):
         self.labelLength.setText(str(self.lengthScrollBar.value()))
 
     def display_password(self):
-        self.words_password()
+        pass
 
     def uppercase(self):
         pass
